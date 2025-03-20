5b76d95736e92a373fcbaa39fe7d9aaeac7eaed1
baranbarisaslan <baranbarisaslan@hotmail.com>
2025-03-20 10:46:07 +0300

    Project Created

---
+++ JekirdekCRM/JekirdekCRM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JekirdekCRM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
